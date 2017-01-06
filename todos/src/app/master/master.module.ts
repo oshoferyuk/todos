cd1a79524d27e98c625bf11e48d4b5a04d305fd6
oshoferyuk <oshoferyuk@gmail.com>
2017-01-06 18:56:04 +0200

    angularfire2 init

--- todos/src/app/master/master.module.ts
+++ todos/src/app/master/master.module.ts
@@ -6,12 +6,15 @@ import { MasterComponent } from './master.component';
 import { EditComponent } from './edit/edit.component';
 import { DetailComponent } from './detail/detail.component';
 import { masterRouting } from './master.routing';
+import { AngularFireModule } from  'angularfire2/index';
+import { firebaseConfig } from '../../environments/firebase.config';
 
 @NgModule({
 declarations: [MasterComponent, EditComponent, DetailComponent],
 imports: [
     CommonModule,
     FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     masterRouting]
 })
 export class MasterModule{
---
+++ todos/src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+      apiKey: "AIzaSyC13zQkcoQgTFcbwDRBD8I-39U5bQyStFI",
+      authDomain: "todo-db834.firebaseapp.com",
+      databaseURL: "https://todo-db834.firebaseio.com",
+      storageBucket: "todo-db834.appspot.com",
+      messagingSenderId: "586584651292"
+}
