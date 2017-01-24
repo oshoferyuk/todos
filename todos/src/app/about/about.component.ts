702c6efe92031193bf7e086f07dcc466388d8cd5
oshoferyuk <oshoferyuk@gmail.com>
2017-01-24 15:42:53 +0200

    provide values example to about section

--- todos/src/app/about/about.component.ts
+++ todos/src/app/about/about.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ComponentCanDeactivate } from './about.guard';
+import { CONFIG1, CONFIG2 } from './values';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  providers: [
+    { provide: CONFIG1, useValue: CONFIG2 }
+  ]
 })
 export class AboutComponent implements OnInit, ComponentCanDeactivate {  
   
 	private isConf: boolean = true;
   
-  constructor(private router:Router) { }
+  constructor(private router:Router, @Inject(CONFIG1) public appConfig:any ) {
+    console.log(this.appConfig); //== bbb, not aaa, overriten by provider
+   }
 
   ngOnInit() {
   }
---
+++ todos/src/app/about/values.ts
@@ -0,0 +1,2 @@
+export const CONFIG1: string = "aaa";
+export const CONFIG2: string = "bbb";
